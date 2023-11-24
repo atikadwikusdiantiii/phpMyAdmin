b52030a68f3a02f0685c6cf01b10b0454bc61f39
atikadwikusdiantiii <148297407+atikadwikusdiantiii@users.noreply.github.com>
2023-11-24 13:26:49 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Membuat Login Multi User Level Dengan PHP dan MySQLi - www.malasngoding.com</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+  <h1>Membuat Login Multi User Level Dengan PHP dan MySQLi <br/> www.malasngoding.com</h1>
+
+  <?php  
+  if(isset($_GET['pesan'])){
+     if($_GET['pesan']=="gagal"){
+        echo "<div class='alert'>username dan password tidak sesuai !</div>";
+     }
+} 
+?>
+
+<div class="kotak_login">
+ <p class="tulisan_login">Silahkan Login</p>
+
+
+ <form action="cek_login.php" method="post">
+   <label>Username</label>
+   <input type="text" name="username" class="form_login" placeholder="username.." required="required">
+
+   <label>Password</label>
+   <input type="password" name="password" class="form_login" placeholder="password .." required="required">
+        
+        <input type="submit" class="tombol_login" value="LOGIN">
+        
+ <br/>
+ <br/>
+<center>
+ <a class="link" href="https://www.malasngoding.com">Kembali</a>
+  </center>
+ </from>
+
+</div>
+
+
+</body>
+</html>
